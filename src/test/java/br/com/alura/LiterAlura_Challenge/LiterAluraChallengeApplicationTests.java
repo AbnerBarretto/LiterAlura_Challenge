665df14ed4faac1907dd4f58877da437f8f32dd9
Abner Barreto <abnerlc4@gmail.com>
2025-08-07 18:34:18 -0300

    Initial commit

---
+++ src/test/java/br/com/alura/LiterAlura_Challenge/LiterAluraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.LiterAlura_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
